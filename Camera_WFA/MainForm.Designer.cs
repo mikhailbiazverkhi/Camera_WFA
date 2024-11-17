25eb58ef734555511116e04546fe27c1f6372ed5
Mikhail <mikhail.biazverkhi@gmail.com>
2024-11-17 12:18:07 -0500

    Initial commit

---
+++ Camera_WFA/MainForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Camera_WFA
+{
+    partial class pictureBox
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mainPictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)mainPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // mainPictureBox
+            // 
+            mainPictureBox.Location = new Point(65, 1);
+            mainPictureBox.Name = "mainPictureBox";
+            mainPictureBox.Size = new Size(654, 437);
+            mainPictureBox.TabIndex = 0;
+            mainPictureBox.TabStop = false;
+            // 
+            // pictureBox
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(mainPictureBox);
+            Name = "pictureBox";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)mainPictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox mainPictureBox;
+    }
+}
---
+++ Camera_WFA/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Windows.Forms;
+
+
+namespace Camera_WFA
+{
+    public partial class pictureBox : Form
+    {
+        private VideoCapture _camera;
+        private double brightnessFactor = 1.0; // Коэффициент яркости
+        private Mat _frame;
+
+        public pictureBox()
+        {
+            InitializeComponent();
+            //_camera = new VideoCapture(0); // Открываем камеру ноутбука
+            _camera = new VideoCapture(1); // Открываем тестовую USB камеру
+            if (!_camera.IsOpened)
+            {
+                MessageBox.Show("Не удалось открыть камеру.");
+                return;
+            }
+
+            // Устанавливаем разрешение
+            _camera.Set(Emgu.CV.CvEnum.CapProp.FrameWidth, 640);
+            _camera.Set(Emgu.CV.CvEnum.CapProp.FrameHeight, 480);
+
+            //_camera.Set(Emgu.CV.CvEnum.CapProp.FrameWidth, 1200);
+            //_camera.Set(Emgu.CV.CvEnum.CapProp.FrameHeight, 800);
+
+            _frame = new Mat();
+            _camera.ImageGrabbed += ProcessFrame; // Подписываемся на событие, когда кадр захвачен
+            _camera.Start();
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_camera.IsOpened)
+            {
+                _camera.Retrieve(_frame);
+                ApplyBrightness(_frame);
+                DisplayFrame(_frame);
+            }
+        }
+
+        private void ApplyBrightness(Mat frame)
+        {
+            // Создаем временную матрицу для хранения результата
+            using (Mat temp = new Mat())
+            {
+                // Умножаем пиксели кадра на коэффициент яркости
+                CvInvoke.Multiply(frame, new ScalarArray(new MCvScalar(brightnessFactor, brightnessFactor, brightnessFactor)), temp);
+
+                // Копируем результат обратно в исходный frame
+                temp.CopyTo(frame);
+            }
+        }
+
+
+        private void DisplayFrame(Mat frame)
+        {
+            // Отображаем кадр в PictureBox
+            mainPictureBox.Image = frame.ToImage<Bgr, byte>().ToBitmap();
+        }
+
+        // Событие для изменения яркости при нажатии клавиш
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Add) // '+' увеличивает яркость
+            {
+                brightnessFactor += 0.1;
+                Console.WriteLine($"Увеличение яркости: {brightnessFactor}");
+            }
+            else if (keyData == Keys.Subtract) // '-' уменьшает яркость
+            {
+                brightnessFactor = Math.Max(0, brightnessFactor - 0.1); // Ограничение на минимальное значение 0
+                Console.WriteLine($"Уменьшение яркости: {brightnessFactor}");
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        // Форма закрывается
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _camera.Stop();
+            _camera.Dispose();
+            CvInvoke.DestroyAllWindows(); // Закрыть все окна OpenCV
+        }
+    }
+}
